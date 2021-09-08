47f470b2928844fbbaaca699c8b8557e08052c43
Kenneth Ibarra Vargas <kiv19022000@gmail.com>
2021-09-07 21:08:12 -0600

    DAO connection

--- app/src/main/java/com/example/courseorganizer/controller/dao/DAO.kt
+++ app/src/main/java/com/example/courseorganizer/controller/dao/DAO.kt
@@ -1,9 +1,17 @@
 package com.example.courseorganizer.controller.dao
 
 import com.example.courseorganizer.model.Usuario
+import java.sql.Connection
+import java.sql.DriverManager
 
 class DAO {
     // private var myconnection: ?? = ??
+
+    var url = "mysql-49257-0.cloudclusters.net"
+    var username = "Test"
+    var password = "12345678"
+
+    var connection: Connection = DriverManager.getConnection(url, username, password)
 
     fun iniciarSesion(usuario:Usuario){
         
